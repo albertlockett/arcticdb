e960ee626506c94597c012d6fc31dae6c8c3854d
albertlockett <albert.lockett@gmail.com>
2022-05-16 20:00:45 -0400

    cleaned up the code a bit and handles bool expr w/ and

--- query/logicalplan/validate.go
+++ query/logicalplan/validate.go
@@ -1,23 +1,43 @@
 package logicalplan
 
 import (
-	"errors"
+	"log"
 	"reflect"
+	"strings"
 )
 
-func Validate(plan *LogicalPlan) error {
+// ExprValidationError is the error for an invalid expression that was found during validation.
+type ExprValidationError struct {
+	message  string
+	expr     Expr
+	children []*ExprValidationError
+}
+
+// ExprValidationError.Error implements the error interface.
+func (err *ExprValidationError) Error() string {
+	return err.message
+}
+
+type LogicalPlanValidator struct {
+	plan *LogicalPlan
+}
+
+// Validate validates the logical plan.
+func (v *LogicalPlanValidator) Validate() error {
+	// TODO here we could check that only one field is set on the plan
+
 	switch {
-	case plan.SchemaScan != nil:
+	case v.plan.SchemaScan != nil:
 		return nil
-	case plan.TableScan != nil:
+	case v.plan.TableScan != nil:
 		return nil
-	case plan.Filter != nil:
-		return ValidateFilter(plan)
-	case plan.Distinct != nil:
+	case v.plan.Filter != nil:
+		return v.ValidateFilter()
+	case v.plan.Distinct != nil:
 		return nil
-	case plan.Projection != nil:
+	case v.plan.Projection != nil:
 		return nil
-	case plan.Aggregation != nil:
+	case v.plan.Aggregation != nil:
 		return nil
 	default:
 		// TODO return error of unsupported plan
@@ -25,12 +45,17 @@ func Validate(plan *LogicalPlan) error {
 	}
 }
 
-func ValidateFilter(plan *LogicalPlan) error {
-	filter := plan.Filter
+// ValidateFilter validates the logical plan's filter step.
+func (v *LogicalPlanValidator) ValidateFilter() error {
+	return v.ValidateFilterExpr(v.plan.Filter.Expr)
+}
 
-	switch expr := filter.Expr.(type) {
+// ValidateFilterExpr validates filter's expression.
+func (v *LogicalPlanValidator) ValidateFilterExpr(e Expr) *ExprValidationError {
+	switch expr := e.(type) {
+
 	case BinaryExpr:
-		return ValidateBinaryExpr(&expr)
+		return v.ValidateFilterBinaryExpr(&expr)
 	default:
 		// TODO does this mean it's unknown - log or do nothing
 	}
@@ -38,16 +63,62 @@ func ValidateFilter(plan *LogicalPlan) error {
 	return nil
 }
 
-func ValidateBinaryExpr(expr *BinaryExpr) error {
-	// enforce that that the left side of the binary expression must be a column
-	// TODO check if this should even be a rule
-	typeFinder := NewTypeFinder((*Column)(nil))
-	expr.Left.Accept(&typeFinder)
-	if typeFinder.result == nil {
-		// TODO it's a validation error
-		return errors.New("AHHHHHHHHHH")
+// ValidateFilterBinaryExpr validates the filter's binary expression.
+func (v *LogicalPlanValidator) ValidateFilterBinaryExpr(expr *BinaryExpr) *ExprValidationError {
+	if expr.Op == AndOp {
+		return v.ValidateFilterAndBinaryExpr(expr)
 	}
 
+	typeFinder := NewTypeFinder((*Column)(nil))
+	expr.Left.Accept(&typeFinder)
+	if typeFinder.result != nil {
+		columnExpr := typeFinder.result.(Column)
+		schema := v.plan.InputSchema()
+		column, found := schema.ColumnByName(columnExpr.ColumnName)
+		if found {
+			// TODO do something with column
+			log.Printf("%v", column)
+
+		} else {
+			// TODO - should we add a rule that the column must exist in the schema?
+		}
+	} else {
+		// TODO - should we add a rule that the left side must be a column?
+	}
+
+	return nil
+}
+
+//ValidateFilterAndBinaryExpr validates the filter's binary expression if the
+// expression is and-ing two other binary expressions together.
+func (v *LogicalPlanValidator) ValidateFilterAndBinaryExpr(expr *BinaryExpr) *ExprValidationError {
+	leftErr := v.ValidateFilterExpr(expr.Left)
+	rightErr := v.ValidateFilterExpr(expr.Right)
+
+	if leftErr != nil || rightErr != nil {
+		message := make([]string, 0, 3)
+		message = append(message, "invalid children:")
+
+		validationErr := ExprValidationError{
+			expr:     expr,
+			children: make([]*ExprValidationError, 0),
+		}
+
+		if leftErr != nil {
+			lve := leftErr
+			message = append(message, "left")
+			validationErr.children = append(validationErr.children, lve)
+		}
+
+		if rightErr != nil {
+			lve := rightErr
+			message = append(message, "right")
+			validationErr.children = append(validationErr.children, lve)
+		}
+
+		validationErr.message = strings.Join(message, " ")
+		return &validationErr
+	}
 	return nil
 }
 
--- query/logicalplan/valitate_test.go
+++ query/logicalplan/valitate_test.go
@@ -2,10 +2,13 @@ package logicalplan
 
 import (
 	"github.com/stretchr/testify/require"
+	"log"
 	"testing"
 )
 
+// TODO rename this file it's spelled wrong
 // TODO check if this should even be a rule
+
 func TestFilterBinaryExprLeftSideMustBeColumn(t *testing.T) {
 	plan := (&Builder{}).
 		Filter(BinaryExpr{
@@ -17,4 +20,25 @@ func TestFilterBinaryExprLeftSideMustBeColumn(t *testing.T) {
 
 	err := Validate(plan)
 	require.NotNil(t, err)
+}
+
+func TestFilterAndExprEvaluatesEachAndedRule(t *testing.T) {
+	plan := (&Builder{}).
+		Filter(And(
+			BinaryExpr{
+				Left:  Col("a"),
+				Op:    EqOp,
+				Right: Literal("b"),
+			},
+			BinaryExpr{
+				Left:  Literal("a"),
+				Op:    EqOp,
+				Right: Literal("b"),
+			},
+		)).
+		Build()
+
+	err := Validate(plan)
+	log.Printf("%v", err) // TODO remove this
+	require.Nil(t, err)
 }
